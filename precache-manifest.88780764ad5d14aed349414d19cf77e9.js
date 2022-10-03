206c7cfd4f2ecbbf13a2ed576c44c79c5290e983
JoshuaKiplimo <joshuakiprono8@gmail.com>
2022-10-02 20:42:24 -0400

    Updates

---
+++ precache-manifest.88780764ad5d14aed349414d19cf77e9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f96ac93ffad1b2a7edf3b49191422c76",
+    "url": "/stripeSubmenu/index.html"
+  },
+  {
+    "revision": "2f4d4e96c6c0953baef6",
+    "url": "/stripeSubmenu/static/css/main.a2467f1d.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripeSubmenu/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripeSubmenu/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f4d4e96c6c0953baef6",
+    "url": "/stripeSubmenu/static/js/main.b9ad5d27.chunk.js"
+  },
+  {
+    "revision": "c3ad1262665a941c5e4d",
+    "url": "/stripeSubmenu/static/js/runtime-main.13864a96.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripeSubmenu/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripeSubmenu/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripeSubmenu/static/media/phone.58d7e3d6.svg"
+  }
+]);
